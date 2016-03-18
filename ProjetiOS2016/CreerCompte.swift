513144acc1ebeeb3438b7cbe206fbe62487100df
tp <tp@tps-macbook-pro.local>
2016-03-18 11:05:23 +0100

    Ajout de la fonctionnalité permettant de créer un compte

---
+++ ProjetiOS2016/CreerCompte.swift
@@ -0,0 +1,61 @@
+//
+//  CreerCompte.swift
+//  ProjetiOS2016
+//
+//  Created by tp on 18/03/2016.
+//  Copyright © 2016 tp. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+class CreerCompte: UIViewController {
+
+    @IBOutlet weak var creerCompte: UIButton!
+    @IBOutlet weak var seConnecter: UIButton!
+    @IBOutlet weak var confirmPassword: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var prenom: UITextField!
+    @IBOutlet weak var nom: UITextField!
+    
+    @IBAction func CreerCompte(sender: AnyObject) {
+        if(password.text == confirmPassword.text){
+            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            let managedContext = appDelegate.managedObjectContext
+            let entity = NSEntityDescription.entityForName("Utilisateur", inManagedObjectContext:managedContext)
+            let utilisateur = NSManagedObject(entity:entity!, insertIntoManagedObjectContext: managedContext)
+            utilisateur.setValue(username.text, forKey: "username")
+            utilisateur.setValue(password.text, forKey: "password")
+            
+            do{
+                try managedContext.save()
+            }catch{
+                print("erreur")
+            }
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
